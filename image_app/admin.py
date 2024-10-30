8ec3a32e736f5c59be97b9c65b55cebf51010a4a
Farezii <fpcrezende@gmail.com>
2024-10-30 09:05:49 -0400

    Added admin page for images

--- image_app/admin.py
+++ image_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('given_id', 'image_path', 'created_at')
+    search_fields = ('given_id', 'created_at')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    
+admin.site.register(Image, ImageAdmin)
