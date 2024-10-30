ef36fa814ef0ecd35dca4ea8bea0ae6f83fbf01c
Farezii <fpcrezende@gmail.com>
2024-10-30 09:02:54 -0400

    Updated URLs for static and media, added image model

--- image_app/models.py
+++ image_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Image(models.Model):
+    image_path = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(default=timezone.now)
