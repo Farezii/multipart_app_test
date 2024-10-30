7b2540caecb9a6e278134bb6b031ded6d59cefd3
Farezii <fpcrezende@gmail.com>
2024-10-30 08:42:40 -0400

    Added app to settings and superuser

--- multipart_app_test/settings.py
+++ multipart_app_test/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # basic imports
     'rest_framework',
+    'image_app',
     # django cleanup of files after deletion
     'django_cleanup.apps.CleanupConfig',
 ]
