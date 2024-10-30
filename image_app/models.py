959aad4a70d2b20d27c87961432715fc30189ca5
Farezii <fpcrezende@gmail.com>
2024-10-30 09:03:51 -0400

    Added app id field

--- image_app/models.py
+++ image_app/models.py
@@ -3,5 +3,6 @@ from django.utils import timezone
 from django.conf import settings
 
 class Image(models.Model):
+    given_id = models.CharField(max_length=150)
     image_path = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(default=timezone.now)
